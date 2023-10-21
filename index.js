f30f927a91a644bab447859b8c17cab84758c552
Aiman-Jannat <ummeaimanjannat@gmail.com>
2023-10-21 06:08:58 +0600

    single element get operation completed

--- index.js
+++ index.js
@@ -60,12 +60,12 @@ async function run() {
     res.send(result);
     })
 
-  //  app.get('/users/:id',async(req,res)=>{
-  //   const id = req.params.id;
-  //   const query = {_id:new ObjectId(id)}
-  //   const user = await userCollection.findOne(query);
-  //   res.send(user);
-  //  })
+   app.get('/users/:id',async(req,res)=>{
+    const id = req.params.id;
+    const query = {_id:new ObjectId(id)}
+    const user = await userCollection.findOne(query);
+    res.send(user);
+   })
 
   //  app.delete('/users/:id', async(req, res)=>{
 
