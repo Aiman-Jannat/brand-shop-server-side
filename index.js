659653b9ea15fdbd5d8b971ceb4083c7b59e7b2d
Aiman-Jannat <ummeaimanjannat@gmail.com>
2023-10-21 21:02:24 +0600

    vercel problem

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(express.json())
+
 app.use(cors());
 
 require('dotenv').config();
@@ -12,7 +12,7 @@ require('dotenv').config();
 
 
 const { MongoClient, ServerApiVersion, Db, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}7bH0BpIUSYOLX9r5@cluster0.tzvbxiw.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tzvbxiw.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
